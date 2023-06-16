d9218e4a9bc4a3d4eb33188f01a0cc4c5254bd4e
Sharon Babu <sharonbabu107@gmail.com>
2023-06-16 15:19:48 +0530

    counter app react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  //creating state
+  const[counter,setCounter]=useState(0)
+  function increment(){
+    setCounter(counter+1)
+  }
+  function decrement(){
+    if(counter!=0){
+      setCounter(counter-1 )
+    }
+
+  }
+  function reset(){
+    setCounter(0)
+  }
   return (
-    <div className="App">
+    <div
+    
+    style={{
+      display:'flex',
+      flexDirection:'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      border: '1px solid black',
+      borderRadius:'20px',
+      padding:'100px',
+      margin: '100px'
+    }}
+
+    className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2 style={{margin:'20px',textAlign:'center',color:'red'}}>Counter App</h2>
+        <h1 style={{margin:'20px',textAlign:'center'}}>{counter}</h1>
+        <button style={{margin:'10px',padding:'10px',  borderRadius:'20px'}} onClick={increment}>Increment</button>
+        <button style={{margin:'10px',padding:'10px',  borderRadius:'20px'}} onClick={decrement}>Decrement</button>
+        <button style={{margin:'10px',padding:'10px',  borderRadius:'20px'}} onClick={reset}>Reset</button>
       </header>
     </div>
   );
